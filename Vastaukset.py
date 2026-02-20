5d48684d4d5480e874d33a1054a5cd1573a67a98
henriikka-raappana <henriikka.raappana1@gmail.com>
2026-02-20 10:41:59 +0200

    Vastaukset.py

--- Vastaukset.py
+++ Vastaukset.py
@@ -29,16 +29,116 @@
 # T5
 # henkilo = input("Mikäs sun nimi on? ")
 # print("Morjesta sulle!", henkilo)
+#
 # sukunimi = input("Entäpäs sukunimi? ")
 # print("Kiitos kiitos", sukunimi + " " + henkilo)
+#
 # postinumero = input("postinumero?? ")
 # print("mmm yeeesss, okok eli ", postinumero)
+#
 # postitoimipaikka = input("vai että semmosta, mikäs postitoimipaikka? ")
 # print("kiitosta tästä,", postitoimipaikka)
 # print(" on rekisteröity")
+#
 # sahkoposti = input("Entäpä vielä sähköposti? O.o ")
 # print("HEHE HÖLÖMÖ!", sahkoposti)
 # print("on nyt tallessa!")
+#
 # print("Minullapa on nyt kaikki sun tiedot!", henkilo + " "  + sukunimi + " "  + postinumero + " "  + postitoimipaikka + " "  + sahkoposti)
 
 # T6
+# nimi = "Lasse"
+# print("Syrjäisessä laaksossa asui nuorukainen", nimi + ",")
+# print("joka oli ammatiltaan lammaspaimen.")
+# print(nimi + " oli vasta herännyt,")
+# print("kun pihaan ratsasti tumma-asuinen ritari.")
+# print("Pian " + nimi + " saisi kuulla, että hänet oli valittu tärkeään tehtävään...")
+
+ # T7
+# luku1 = 5
+# luku2 = 4
+# summa = luku1 + luku2
+# print(luku1, "+", luku2, "=", summa)
+
+# luku3 = 22
+# luku4 = 8
+# erotus = luku3 - luku4
+# print(luku3, "-", luku4, "=", erotus)
+
+# luku5 = 2
+# luku6 = 8
+# kerto = luku5 * luku6
+# print(luku5, "*", luku6, "=", kerto)
+
+# luku7 = 22
+# luku8 = 8
+# jako = luku7 / luku8
+# print(luku7, "/", luku8, "=", jako)
+
+ # T8
+# luku1 = float(input("Anna luku: "))
+# luku2 =float(input("Anna toinen luku: "))
+# print("summa", luku1 + luku2)
+#
+# luku1 = float(input("Anna luku: "))
+# luku2 =float(input("Anna toinen luku: "))
+# print("summa", luku1 - luku2)
+#
+# luku1 = float(input("Anna luku: "))
+# luku2 =float(input("Anna toinen luku: "))
+# print("summa", luku1 * luku2)
+#
+# luku1 = float(input("Anna luku: "))
+# luku2 =float(input("Anna toinen luku: "))
+# print("summa", luku1 / luku2)
+
+ # T9
+# nimi = "Hyvä asiakas"
+# tervehdys = "Tervetuloa sairaalaan, "
+# viesti = tervehdys + nimi
+# print(viesti)
+# print(len(viesti))
+
+ # T9,5
+# nimi = "Teija Testaaja"
+# ika = "17"
+# taito1 = "Python (aloittelija)"
+# taito2 = "Web-ohjelmointi (veteraani)"
+# taito3 = "Ohjelmointi (puoliammattilainen)"
+# luku = "2000 - 3000"
+# print("Nimeni on " + nimi + ", olen " + ika + " vuotias")
+# print("Taitoihini kuuluvat " + taito1 + " ja " + taito2 + " sekä " + taito3)
+# print("Ja haen työtä, josta maksetaan palkaa " + luku + " euroa kuukaudessa.")
+
+ # T10
+# x = int(input("Anna ensimmäinen luku: "))
+# y = int(input("Anna toinen luku: "))
+# summa = x + y
+# print(summa)
+
+ # T11
+# kirjain = input("Anna kirjain: ")
+# print("Syötit kirjaimen:", kirjain)
+
+ # T12
+# nimi1 = input("Kerro nimesi: ")
+# ika1 = int(input("Kerro ikäsi: "))
+#
+# nimi2 = input("Kerro nimesi: ")
+# ika2 = int(input("Kerro ikäsi: "))
+#
+# yhteensa = ika1 + ika2
+#
+# print(nimi1, "ja", nimi2, "ovat yhteensä", yhteensa, "vuotta vanhoja")
+
+ # T13
+# viesti = "Olen "
+# luku = 25
+# print(viesti + str(luku) + " vuotta vanha")
+
+ # T13,5
+# henkilo = input("Nimesi?: ")
+# kenka = input("Kengän kokosi?: ")
+# print("Nimesi on " + henkilo + " ja kengän kokosi on " + kenka)
+
+ # T1 - 13 Oma koodi
